9610b8de47d03044dc97e689037f0588fc75c867
wwx <weixiong_birnary@foxmail.com>
2020-03-25 19:32:08 +0800

    load the earth

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import { Viewer } from 'cesium';
 
 @Component({
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private mounted(): void {
+    this.$nextTick(() => {
+        const viewer = new Viewer('app');
+    });
+  }
+}
 </script>
 
 <style lang="scss">
@@ -24,6 +30,10 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  // margin-top: 60px;
+}
+
+.cesium-viewer {
+  position: absolute !important;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// noinspection ES6UnusedImports
+// import '../node_modules/cesium/Source/Widgets/widgets.css';
+import 'cesium/Source/Widgets/widgets.css';
 
 Vue.config.productionTip = false;
 
